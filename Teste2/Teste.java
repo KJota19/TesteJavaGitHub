264280485cfd84232e7b9a616fdbdc6346eb472e
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-03-26 16:33:51 -0300

    .

---
+++ Teste2/Teste.java
@@ -0,0 +1,27 @@
+package Teste2;
+
+public class Teste {
+    public static void main(String[] args){;
+        //tipos primitivos
+
+        int age=19;
+        int idade=(int)100000000L;//colocando um número maior em um int que não comporta
+        long numeroGrande2=(long)155.23;
+        long numeroGrande=10000L;
+        double salarioDouble= 2000.0D;
+        float salarioFloat= 2500.0F;
+        byte idadeByte=127;//ou-127,-128...
+        short idadeShort=32000;
+        boolean verdadeiro=true;
+        boolean falso=false;
+        char caractere = 'M';//ou um numero
+        String nome = "um grande texto";
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(numeroGrande2);
+        System.out.println("the age is: " + age + " anos ");
+        System.out.println(false);
+        System.out.println("char"+caractere);
+
+    }
+}

a12fcc479efc81cf52faf857fb1c56b9eabd524e
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 16:29:30 -0300

    Create Teste Java GitHub4.java

---
+++ Operadores/Teste04.java
@@ -0,0 +1,19 @@
+package Operadores;
+
+import java.util.Scanner;
+
+public class Teste04 {
+    public static void main(String[] args) {
+    Scanner leitor = new Scanner (System.in);
+
+     System.out.println("Digite um valor em metros:");
+     int metro = leitor.nextInt();
+     
+     int dm = metro * 10;
+     int cm = dm * 10;
+     int mm = cm * 10;
+     
+     System.out.println("O valor em centimetros é: " + cm);
+     System.out.println("O valor em milimetros é: " + mm); 
+    }
+}

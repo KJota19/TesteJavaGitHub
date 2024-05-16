9d68e697ea733625ad00fb80de6b2295855d23e7
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 16:29:16 -0300

    Create Teste Java GitHub3.java

---
+++ Operadores/Teste03.java
@@ -0,0 +1,21 @@
+package Operadores;
+
+import java.util.Scanner;
+
+public class Teste03 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        
+        System.out.println("Digite um número : ");
+        int numero = leitor.nextInt();
+        
+        boolean isNumeroPositivo = numero > -1;
+        boolean isNumeroNegativo = numero < 0;
+        boolean isNumeroZero = numero == 0;
+
+        System.out.println("O número é positivo ? " + isNumeroPositivo);
+        System.out.println("O número é negativo ? " + isNumeroNegativo);
+        System.out.println("O número é igual a zero ? " + isNumeroZero);
+    }
+    
+}

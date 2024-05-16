d51631eabc48a8b4de0b53897eab5165ac3846cb
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 16:31:42 -0300

    Create Teste Java GitHub3.java

---
+++ TiposPrimitivos/Teste03.java
@@ -0,0 +1,17 @@
+// balanço trimestral e média mensal de uma empresa.
+
+package TiposPrimitivos;
+
+public class Teste03 {
+    public static void main(String[] args) {
+        int janeiro=15000;
+        int fevereiro=23000;
+        int marco=17000;
+        int gastosTrimestre=janeiro+fevereiro+marco;
+        System.out.println("O balanço trimestral da empresa: " + gastosTrimestre);
+        //System.out.println("O balanço trimestral da empresa: " + (janeiro+fevereiro+marco));
+
+        int mediaMensal=gastosTrimestre/3;
+        System.out.println("valor da média mensal = " + mediaMensal);
+    }
+}

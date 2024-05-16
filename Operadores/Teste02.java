0293af69f4c629afffa3883cc004a1d490e31462
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 16:29:03 -0300

    Create Teste Java GitHub2.java

---
+++ Operadores/Teste02.java
@@ -0,0 +1,17 @@
+package Operadores;
+
+import java.util.Scanner;
+public class Teste02 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+        
+        System.out.println("Digite o valor do raio do circulo:");
+        float raio = leitor.nextFloat();
+
+        float area = 3.14f * (raio*raio);
+        float perim = 2 * 3.14f * raio;
+        
+        System.out.println("a area do circulo: " + area);
+        System.out.println("o perimetro do circulo: " + perim);
+    }
+}

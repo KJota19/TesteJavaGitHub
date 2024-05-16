1bb66d4ce136661af03f8348da14a1e1dc1f069a
KJota2004 <160192773+KJota2004@users.noreply.github.com>
2024-05-16 16:32:01 -0300

    Create Teste Java GitHub5.java

---
+++ TiposPrimitivos/Teste05.java
@@ -0,0 +1,17 @@
+//nome e idade
+
+package TiposPrimitivos;
+
+import java.util.Scanner;
+public class Teste05 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("digite seu nome: ");
+        String nome=leitor.nextLine();
+        System.out.println("digite sua idade:");
+        int idade=leitor.nextInt();
+        System.out.println("Seu nome é " + nome + " e você tem " + idade + " anos.");
+
+      }
+    
+}
